78b66bdd274a70403f40ebf51eaa3aee13c2dee7
elbaro <elbaro@users.noreply.github.com>
2023-05-27 04:52:44 +0900

    wip: list

--- src/list.rs
+++ src/list.rs
@@ -1,32 +1,48 @@
 // ## List<T: ToPyObject>
+// TODO: move to separate lib
 
-// struct BorrowedList<Owner, Item>(OwningRef<Owner, [Item]>);
+pub struct BorrowedList<Item, Borrowed> {
+    // Item=Tweet, Borrowed=TweetBorrowed
+    owner: Arc<dyn Any + Send + Sync>,
+    slice: &'static [Item],
+}
 
-// struct BorrowedList<Owner, Borrowed> {
-//   owner: Arc<Owner>,
-//   borrowed: &'this [Borrowed],
-// }
+#[pyclass]
+pub struct ProxyList(Box<dyn ProxyListTrait>);
 
-// impl ProxyListTrait for BorrowedList<Owner, Item> {
-//   fn len (&self) -> usize{ self.0.len() }
-//   fn get<'py>(&self, idx: usize, py: Python<'py>) -> &'py PyAny { self.0[i] }
-//   <!-- fn slice(&self, selector: ()) -> Box<dyn ProxyListTrait> {  -->
-//     <!-- self.0.clone().map(|slice| slice[selector]) -->
-//     <!-- Box::new(BorrowedList(self.0.clone())) -->
-//   <!-- } -->
-// }
+#[pymethod]
+impl ProxyList {
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+    pub fn get(&self, idx: usize) -> &PyAny {
+        self.0.get(idx)
+    }
+    pub fn iter() {}
+}
 
-// #[pyclass]
-// struct ProxyList(Box<dyn ProxyListTrait>) {
+pub trait ProxyListTrait {
+    fn len(&self) -> usize;
+    fn get<'py>(&self, idx: usize, py: Python<'py>);
+    fn iter();
+}
 
-// }
+// impl ProxyListTrait for [i64] {}
+// impl ProxyListTrait for [i32] {}
 
-// #[pymethod]
-// impl ProxyList {
-//   pub fn len(&self) -> usize {
-//     self.0.len()
-//   }
-//   pub fn get(&self) -> &PyAny {
-//     self.0.get()
-//   }
+// impl ProxyListTrait for BorrowedList<Item, Borrowed> {
+//     fn len(&self) -> usize {
+//         self.slice.len()
+//     }
+//     fn get<'py>(&self, idx: usize, py: Python<'py>) -> &'py PyAny {
+//         self.slice[i]
+//         Borrowed {
+//             owner: ,
+//             borrowed: ,
+//         }
+//     }
+//     // fn slice(&self, selector: ()) -> Box<dyn ProxyListTrait> {
+//     //   self.0.clone().map(|slice| slice[selector])
+//     //    Box::new(BorrowedList(self.0.clone()))
+//     // }
 // }
